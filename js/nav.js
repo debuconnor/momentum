e53ef8200cd7ec818f5954681b89339533b2eca9
debuconnor <gterppw@gmail.com>
2022-03-20 20:51:40 +0900

    cancel creating calendar section

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,9 @@
 const navEl                 = document.querySelector("nav")
 const navClockEl            = navEl.querySelector(".nav-clock");
 const navTodoEl             = navEl.querySelector(".nav-todo");
-const navCalendarEl         = navEl.querySelector(".nav-calendar");
 const selectorEl            = document.getElementById("selector");
 const todoBoxEl             = document.querySelector(".todoBox");
 const clockBoxEl            = document.querySelector(".clockBox");
-const calendarBoxEl         = document.querySelector(".calendarBox");
 const containerEl           = document.querySelector(".main.container");
 const HIDDEN_CLASSNAME      = 'hidden';
 
@@ -28,25 +26,16 @@ function onElementClick(e){
         if(activeNav.indexOf('nav-clock') !== -1){
             todoBoxEl.classList.add(HIDDEN_CLASSNAME);
             clockBoxEl.classList.remove(HIDDEN_CLASSNAME);
-            calendarBoxEl.classList.add(HIDDEN_CLASSNAME);
-        }
-        else if(activeNav.indexOf('nav-calendar') !== -1){
-            todoBoxEl.classList.add(HIDDEN_CLASSNAME);
-            clockBoxEl.classList.add(HIDDEN_CLASSNAME);
-            calendarBoxEl.classList.remove(HIDDEN_CLASSNAME);
         }
         else if(activeNav.indexOf('nav-todo') !== -1){
             todoBoxEl.classList.remove(HIDDEN_CLASSNAME);
             clockBoxEl.classList.add(HIDDEN_CLASSNAME);
-            calendarBoxEl.classList.add(HIDDEN_CLASSNAME);
         }
         containerEl.style.height = '400px';
     }, 300);
 }
 
 navClockEl.addEventListener("mouseover", onElementMouseover);
-navCalendarEl.addEventListener("mouseover", onElementMouseover);
 navTodoEl.addEventListener("mouseover", onElementMouseover);
 navClockEl.addEventListener("click", onElementClick);
-navCalendarEl.addEventListener("click", onElementClick);
 navTodoEl.addEventListener("click", onElementClick);
